61618cd6cee0834c1752e2f8805120308b6dea8e
Russell Xin <1612650023@qq.com>
2023-09-13 22:09:17 +0800

    位置互换

--- RFrame/Assets/Scripts/GamePlay/GameObjects/Mark.cs
+++ RFrame/Assets/Scripts/GamePlay/GameObjects/Mark.cs
@@ -1,6 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Mark : Bullets
 {
+    private Transform player;
     
+#region Override
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    protected override void OnTrigger(Collider2D go)
+    {
+        if (go.gameObject.layer==LayerMask.NameToLayer("character"))
+        {
+            var transform1 = go.transform;
+            (transform1.position, player.position) = (player.position, transform1.position);
+        }
+    }
+
+#endregion
 }
